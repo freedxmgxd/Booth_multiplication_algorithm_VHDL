1c0f279435af367cf8be7f72cbf4ebd3f4e68e4e
Shinmen <juniorzatchbel@gmail.com>
2021-05-24 19:02:59 -0300

    2021-05-24 Katy Perry - I Kissed A Girl

--- multiplex2.vhd
+++ multiplex2.vhd
@@ -5,3 +5,24 @@ library ieee ;
 use ieee.std_logic_1164.all;
 
 ---------------------------------------------------
+
+entity multiplex2 is
+    port (
+        input1:     in bit_vector(2 downto 0);
+        input2:     in bit_vector(2 downto 0);
+        sel:        in bit;
+        output1:    in bit_vector(2 downto 0)
+    );
+end entity multiplex2;
+
+architecture dentro of multiplex2 is    
+begin
+    process(input1, input2, sel)
+        case sel is
+            when '0' => output1 <= input1;
+            when '1' => output1 <= input2;
+        end case;
+    end process
+    
+    
+end architecture dentro;
