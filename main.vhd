065c30597aa066f2c858372d02cf4eea08d8d97e
Shinmen <juniorzatchbel@gmail.com>
2021-05-26 22:23:51 -0300

    2021-05-26 Olivia Rodrigo - deja vu

--- main.vhd
+++ main.vhd
@@ -13,7 +13,7 @@ port(
     shift:		in std_logic;
     load:       in std_logic;
     reset:      in std_logic;
-    clck:		in std_logic;
+    clckSR:		in std_logic;
     RegOut:		buffer std_logic_vector (4 downto 0)
 );
 end shift_reg;
@@ -26,20 +26,20 @@ architecture behv of shift_reg is
     
 begin
     
-    process(clck, reset)
+    process(clckSR, reset)
     begin
 
    if reset = '1' then 
       S <= "00000";
    end if;
 	-- everything happens upon the clock changing
-	if clck'last_value='1' and clck='0' then
+	if clckSR'last_value='1' and clckSR='0' then
 	    if shift = '1' then
-		   S(0) <= inputS;
-         S(1) <= RegOut(0);
-         S(2) <= RegOut(1);   
-         S(3) <= RegOut(2);
-         S(4) <= RegOut(3);
+		    S(0) <= inputS;
+            S(1) <= RegOut(0);
+            S(2) <= RegOut(1);   
+            S(3) <= RegOut(2);
+            S(4) <= RegOut(3);
 	    end if;
        if load = '1' then
 		   S <= RegIn;
