475e90f895113c5deb49efcad8ffaa14612d89c7
Shinmen <juniorzatchbel@gmail.com>
2021-05-29 11:31:18 -0300

    2021-05-29 LSD - Genius

--- main_tb.vhd
+++ main_tb.vhd
@@ -44,7 +44,7 @@ architecture testbench of tb is
         variable cERR: 	integer := 0;
         variable aux: 	std_logic_vector(7 downto 0);
 
-        -- Case 1: Usando EPWave ou outra ferramenta de waveform, será possivel acompanhar o diagrama temporal deste caso. Ao acompanhar o elemento pOut do design fica temporalmente fica mais facil de acompanhar o algoritmo de booth.
+        -- Case 1
 
         begin
         Rst <= '1';
@@ -65,39 +65,49 @@ architecture testbench of tb is
             
         assert (outP = "11110000") report "Erro no caso 1!" severity error;
 
-        report "resultado: "     & to_string(Outp);
-    
-
-        --Case 2 : Irá verificar para todos os casos possiveis se a solução bate com o esperado.
-    
+        report "resultado: "     & to_string(outp); 
+        
         Rst <= '1';
         wait for 10 ns;
         Rst <= '0';
-        wait for 5 ns;
-        
-        for i in (-8) to 7 loop
-            for j in (-8) to 7 loop
-                sA <= std_logic_vector(to_signed(i, 4));
-                sB <= std_logic_vector(to_signed(j, 4));
-                Stt <= '1';
-                Rst <= '0';
-                wait for 30 ns;
-                Stt <= '0';
-                wait for 150 ns;
-                
-                aux := std_logic_vector(to_signed(j * i, 8));
-                
-                assert (outp = aux) report "Error na operação " & to_string(sA) & " * " & to_string(sB);
-                if (outp /= aux) then
-                cERR := cERR + 1;
-                end if;
+        wait for 20 ns;
+
+
+        assert (outP = "00000000") report "Erro no caso 1!" severity error;
+        report "resultado: "     & to_string(outp); 
+     
+
+    --Case 2
+    
+    Rst <= '1';
+    wait for 10 ns;
+    Rst <= '0';
+    wait for 5 ns;
+    
+    for i in (-8) to 7 loop
+    	for j in (-8) to 7 loop
+        	sA <= std_logic_vector(to_signed(i, 4));
+            sB <= std_logic_vector(to_signed(j, 4));
+			Stt <= '1';
+            Rst <= '0';
+            wait for 30 ns;
+            Stt <= '0';
+            wait for 150 ns;
             
+            aux := std_logic_vector(to_signed(j * i, 8));
             
-            end loop;      
-        end loop;
+            assert (outp = aux) report "Error na operação " & to_string(sA) & " * " & to_string(sB);
+            if (outp /= aux) then
+            cERR := cERR + 1;
+            end if;
         
-        report "Todos os testes foram realizados! Numeros de erros encontrados: " & to_string(cERR); 
-        wait;
+        
+        end loop;      
+    end loop;
+    
+    report "Todos os testes foram realizados! Numeros de erros encontrados: " & to_string(cERR); 
+    wait;
 end process;
 
 end testbench;
+
